96c2a14cd453082f8cd7aca48f73ef52cedebbc8
PC-VITO\user <yohanes.vito@ti.ukdw.ac.id>
2021-12-22 03:13:09 +0700

    fix get data

---
+++ IotAkhir/app/src/main/java/id/ac/ukdw/iotakhir/Employee.kt
@@ -0,0 +1,9 @@
+package id.ac.ukdw.iotakhir
+
+data class Employee (
+    val uid:String? = null,
+    val Driver:String? = null,
+    val Registration:String? = null,
+    val Transport:String? = null,
+    val isAuthorized:String? = null
+    )
--- IotAkhir/app/src/main/java/id/ac/ukdw/iotakhir/MonitorActivity.kt
+++ IotAkhir/app/src/main/java/id/ac/ukdw/iotakhir/MonitorActivity.kt
@@ -3,12 +3,18 @@ package id.ac.ukdw.iotakhir
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import com.google.firebase.database.*
 
 class MonitorActivity : AppCompatActivity() {
-    lateinit var ref : DatabaseReference
+
+
+    val uidListGate = ArrayList<String>()
+    val employeeList = ArrayList<Employee>()
+    val timeStampList = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_monitor)
@@ -16,28 +22,84 @@ class MonitorActivity : AppCompatActivity() {
         val txt2 = findViewById<TextView>(R.id.txt2)
         val txt3 = findViewById<TextView>(R.id.txt3)
         var btn = findViewById<Button>(R.id.button2)
-        ref = FirebaseDatabase.getInstance("https://tugas-akhir-iot-b307d-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference()
 
-        ref.addValueEventListener(object :ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val gate1 = snapshot.child("Gate1").getValue().toString()
-                txt1.setText(gate1)
-                val gate2 = snapshot.child("Gate2").getValue().toString()
-                txt2.setText(gate2)
-                val gate3 = snapshot.child("Gate3").getValue().toString()
-                txt3.setText(gate3)
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
-            }
-
-        })
         btn.setOnClickListener {
+            getData()
+            getEmployee()
             val username = getIntent().getStringExtra("username")
             val i = Intent(this, AdminActivity::class.java)
             i.putExtra("username", username.toString())
             startActivity(i);
         }
+    }
+    fun getEmployee(){
+        val firebase = FirebaseDatabase.getInstance().getReference("Access")
+
+        firebase.addValueEventListener(object :ValueEventListener{
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                val uidListAccess = ArrayList<String>()
+
+                //mencari masing masing uid yang berbeda kemudian memasukkan kedalam uidlist
+                for (data in dataSnapshot.children) {
+                    uidListAccess.add(data.key.toString())
+                }
+
+                //mencari masing masing timestamp yang berbeda kemudian memasukkan kedalam uidlist
+                for (i in 0 .. (uidListAccess.size-1)) {
+                    val Driver = dataSnapshot.child("${uidListAccess[i]}/Driver").getValue().toString()
+                    val Registration = dataSnapshot.child("${uidListAccess[i]}/Registration").getValue().toString()
+                    val Transport = dataSnapshot.child("${uidListAccess[i]}/Transport").getValue().toString()
+                    val isAuthorized = dataSnapshot.child("${uidListAccess[i]}/isAuthorized").getValue().toString()
+
+                    employeeList.add(Employee(uidListAccess[i],Driver, Registration,Transport,isAuthorized))
+
+                    Log.d("Driver", "id: ${employeeList[i].Driver}")
+                    Log.d("uid", "id: ${employeeList[i].uid}")
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+    }
+
+    fun getData(){
+
+
+        val firebase = FirebaseDatabase.getInstance().getReference("Gate1")
+
+        firebase.addValueEventListener(object :ValueEventListener{
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+
+                //mencari masing masing uid yang berbeda kemudian memasukkan kedalam uidlist
+                for (data in dataSnapshot.children) {
+                    uidListGate.add(data.key.toString())
+                }
+
+                //mencari masing masing timestamp yang berbeda kemudian memasukkan kedalam uidlist
+                for (i in 0 .. (uidListGate.size-1)) {
+                    val gate1 = dataSnapshot.child("${uidListGate[i]}/TimeStamp").getValue().toString()
+                    timeStampList.add(gate1)
+                    Log.d("lol", "id: ${uidListGate[i]} Timestamp: ${gate1}")
+                }
+
+//                //debugging tools
+//                for (i in 0 .. (uidListGate.size-1)) {
+//                    Log.d("uidList", "id: ${uidListGate[i]}")
+//                }
+//                for (i in 0 .. (timeStampList.size-1)) {
+//                    Log.d("timestamp", "id: ${timeStampList[i]}")
+//                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
     }
 }
